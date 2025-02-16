40e84e3ca9773e597d9348a15dc984400decad42
esther447 <e.mushimiyi@alustudent.com>
2025-02-16 15:01:19 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""DOCS"""
+"""Fetches the top 10 hot posts of a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """Prints the titles of the top 10 hot posts for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        data = response.json().get("data", {})
+        posts = data.get("children", [])
+
+        if not posts:
+            print(None)
+            return
+
+        for post in posts[:10]:
+            print(post["data"]["title"])
     else:
         print(None)
