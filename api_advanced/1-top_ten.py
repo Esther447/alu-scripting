b86396c339ee20d62fe0e594441790f5fe591009
esther447 <e.mushimiyi@alustudent.com>
2025-02-17 00:17:11 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""Module to print the titles of the top 10 hot posts from a given subreddit."""
-
+"""
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}  # Fetch only 10 posts
-
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-
-    data = response.json().get('data', {})
-    children = data.get('children', [])
-
-    if not children:
-        print(None)
-        return
-
-    for post in children:
-        print(post.get('data', {}).get('title', 'No Title'))
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
