1c400542077c1ec09bcf5c76ecf68cc32c1bf8ed
esther447 <e.mushimiyi@alustudent.com>
2025-02-16 14:23:40 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
 """DOC"""
 import requests
 
 
-def number_of_subscibers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-script:v1.0 (by /u/yourusername)'}
+def number_of_subscribers(subreddit):
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
 
     if response.status_code == 200:
-        data = response.json()
-        return data.get("data", {}.get("subscribers", 0)
-    else:
-        return 0
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+    return 0
