b7f87b4e5b49ce069fdcfdbaf247fd902f3b85c3
esther447 <e.mushimiyi@alustudent.com>
2025-02-16 23:53:38 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,11 +7,11 @@ def top_ten(subreddit):
     """Docs"""
     reddit_url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()['data']
+        data = response.json().get['data']
         for post in data['children'][:10]:
             print(post['data']['title'])
     else:
