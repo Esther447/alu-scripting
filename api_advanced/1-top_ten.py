06122470799030a77c85b8009c700a9d6318013b
esther447 <e.mushimiyi@alustudent.com>
2025-02-21 20:37:30 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,34 @@
-#!/usr/bin/python3
-"""DOCS"""
 import requests
 
-
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "MyRedditBot/1.0"}
 
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print(None)
+        elif e.response.status_code == 302:
+            print(None)
+        else:
+          print(None)
+
+    except (requests.exceptions.RequestException, ValueError, KeyError):
         print(None)
